b7d34cd55ba22927360d09878927aa73a8817c40
Serafeim Papastefanos <spapas@gmail.com>
2019-01-02 15:57:28 +0200

    Add comment

--- src/aoc2018/day3.clj
+++ src/aoc2018/day3.clj
@@ -13,6 +13,12 @@
                                      ))))))
 
 ; Same as the 1st parser using the thread-last macro
+; Easier to explain. For each line:
+; * Find the numbers using the reg-ex spec
+; * Ignore the 1st item of the re-find response (it's the matched string)
+; * Convert the number to integers
+; * Interlave the numbers with dictionary keys (so that we'll have a list of "id" 1 "left" 3223 etc )
+; * Pass that list to assoc using apply so that we'll get (assoc {} "id 1 etc)
 (defn parser2[line]
   (->> line
        (re-find #"^#(\d+)\ @\ (\d+),(\d+):\ (\d+)x(\d+)")
