5a1f6d610fc2b237bce8be1de39d856295e2db22
Serafeim Papastefanos <spapas@gmail.com>
2020-08-16 17:13:08 +0300

    Start parsing input

--- src/aoc2018/day4.clj
+++ src/aoc2018/day4.clj
@@ -3,5 +3,11 @@
              [ clojure.set :as set])
   )
 
+(def day4-input (co/get-lines "input/day4"))
+
 (def answer1 42)
 (def answer2 43)
+
+(defn pr-input []
+  (doseq [z (sort day4-input)] (println z))
+   )
