a7f52372348a209d295b4d2530704ed546f499d0
Serafeim Papastefanos <spapas@gmail.com>
2020-08-18 23:17:12 +0300

    Start day5

---
+++ src/aoc2018/day5.clj
@@ -0,0 +1,9 @@
+(ns aoc2018.day5
+  (:require [ aoc2018.common :as co]
+             [ clojure.set :as set])
+  )
+
+(def day5-input (sort (co/get-lines "input/day4")))
+
+(def answer1 43)
+(def answer2 43)
