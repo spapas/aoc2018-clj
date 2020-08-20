f4e918d40e7e1d010d1248d0347cca5b05ded470
Serafeim Papastefanos <spapas@gmail.com>
2020-08-20 23:27:38 +0300

    Start day7

---
+++ src/aoc2018/day7.clj
@@ -0,0 +1,10 @@
+(ns aoc2018.day7
+  (:require [ aoc2018.common :as co]
+            [ clojure.string :as str]
+             [ clojure.set :as set]))
+
+(def day7-input (-> "input/day7" slurp str/trim))
+
+(def answer1 42)
+
+(def answer2 43)
