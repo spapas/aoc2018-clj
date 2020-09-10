c31ffe90de5c01540f070f6d10abbd9d39818247
Serafeim Papastefanos <spapas@gmail.com>
2020-09-10 19:33:34 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [vlaaad/reveal "0.1.0-ea30"]]
   :main ^:skip-aot aoc2018.core
   :target-path "target/%s"
-  :plugins [[cider/cider-nrepl "0.18.0"][org.clojars.benfb/lein-gorilla "0.5.0"]]
+  :plugins [[cider/cider-nrepl "0.25.3"]
+            [org.clojars.benfb/lein-gorilla "0.7.0"]]
   :profiles {:uberjar {:aot :all}})
