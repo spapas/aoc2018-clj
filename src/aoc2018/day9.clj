fc06ebf840e784e96f3e66f15fc350beef88502c
Serafeim Papastefanos <spapas@gmail.com>
2020-09-21 23:07:03 +0300

    Normal cas eok

--- src/aoc2018/day9.clj
+++ src/aoc2018/day9.clj
@@ -9,16 +9,12 @@
 
 (defstruct game :marbles :curr)
 
-(defn place-marble [game marble]
-  game)
-
 
 (defn do-put [l idx v acc]
-  (if (zero? idx) (concat acc (cons v l))
+  (if (zero? idx) (concat (reverse acc) (cons v l))
       (let [f (first l)
             r (rest l)]
         (recur r (dec idx) v (cons f acc)))))
-
 
 (defn put [l idx v]
   (do-put l idx v '()))
@@ -36,3 +32,19 @@
         (if (>= c-p 0)
           c-p
           (mod c-p mc))))))
+
+
+(defn place-marble [g marble]
+  (let [c1 (get-idx g 1 :cw)
+        c2 (get-idx g 2 :cw)
+        marbles (:marbles g)
+        curr (:curr g)]
+    (println c1)
+    (println c2)
+    (cond
+      (= c1 c2) (struct game [0 1] 1)
+      (< c1 c2) (struct game
+                        (put marbles (inc c1) marble)
+                        c2)
+
+      :else (struct game (reverse (cons marble (reverse marbles)))))))
