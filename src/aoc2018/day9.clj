43309d3c56d32d0766ec000225c9b68b499fe2d2
Serafeim Papastefanos <spapas@gmail.com>
2020-09-21 10:12:26 +0300

    Start day 9

---
+++ src/aoc2018/day9.clj
@@ -0,0 +1,32 @@
+(ns aoc2018.day9
+  (:require [aoc2018.common :as co]
+            [clojure.string :as str]
+            [clojure.zip :as z]
+            [clojure.set :as set]))
+
+(defstruct game :marbles :curr)
+
+(defn place-marble [game marble]
+  game)
+
+(defn put [l idx v]
+  (do-put [l idx v '()])
+  )
+
+(defn do-put [l idx v acc ]
+  (if (zero? idx) (concat acc (cons v l))
+      (let [f (first l)
+            r (rest l)]
+        (recur r (dec idx) v (cons f acc)
+        )
+      )
+  )
+
+(defn get-idx [game places dir]
+  (let [marbles (:marbles game)
+        curr (:curr game)
+        mc (count marbles)
+        ]
+  (if (= dir :cw)
+    (if ( > (+ curr places) mc) ())
+    ())))
