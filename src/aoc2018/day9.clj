0eeb90127245a9eccd90230e917e5b148dbbee14
Serafeim Papastefanos <spapas@gmail.com>
2020-09-21 23:42:13 +0300

    Special case seems ok

--- src/aoc2018/day9.clj
+++ src/aoc2018/day9.clj
@@ -33,18 +33,36 @@
           c-p
           (mod c-p mc))))))
 
+(defn get-val [l idx]
+  (if (zero? idx)
+    (first l)
+    (recur (rest l) (dec idx))))
+
+(defn do-del-idx [l idx acc]
+  (if (zero? idx)
+    (concat (reverse acc) (rest l))
+    (recur (rest l) (dec idx) (cons (first l) acc))))
+
+(defn del-idx [l idx]
+  (do-del-idx l idx '()))
 
 (defn place-marble [g marble]
-  (let [c1 (get-idx g 1 :cw)
-        c2 (get-idx g 2 :cw)
-        marbles (:marbles g)
-        curr (:curr g)]
-    (println c1)
-    (println c2)
-    (cond
-      (= c1 c2) (struct game [0 1] 1)
-      (< c1 c2) (struct game
-                        (put marbles (inc c1) marble)
-                        c2)
+  (if (= 0 (mod marble 23))
+    (let [c1 (get-idx g 7 :ccw)
+          marbles (:marbles g)]
+      [(+ marble (get-val marbles c1))
+       (struct game (del-idx marbles c1) c1)])
+    [0
+     (let [c1 (get-idx g 1 :cw)
+           c2 (get-idx g 2 :cw)
+           marbles (:marbles g)
+           curr (:curr g)]
+       (println c1)
+       (println c2)
+       (cond
+         (= c1 c2) (struct game [0 1] 1)
+         (< c1 c2) (struct game
+                           (put marbles (inc c1) marble)
+                           c2)
 
-      :else (struct game (reverse (cons marble (reverse marbles)))))))
+         :else (struct game (reverse (cons marble (reverse marbles))))))]))
