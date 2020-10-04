259d0661149fa190313a0a01aa0589f763d779ec
Serafeim Papastefanos <spapas@gmail.com>
2020-10-04 18:21:30 +0300

    day 11 ok

--- src/aoc2018/day11.clj
+++ src/aoc2018/day11.clj
@@ -22,19 +22,26 @@
         s1 (* rid y)
         s2 (+ s1 grid-sn)
         s3 (* s2 rid)
-        s4 (get-hundreds s3)
-        ]
-     (- s4 5)))
+        s4 (get-hundreds s3)]
+    (- s4 5)))
 
-(def power-levels 
+(def power-levels
   (into {} (for [x (range 300) y (range 300)] [[x y], (power-level [x y])])))
 
-(defn sum-power-levels [x y]
-  (reduce + (for [i (range x (+ 3 x)) j (range y (+ 3 y))]
-   (power-level [i j] ))))
+(defn sum-power-levels [x y n]
+  (reduce + (for [i (range x (+ n x)) j (range y (+ n y))]
+              (power-level [i j]))))
 
-(def power-levels-squares
-  (for [x (range 298) y (range 298)] [[x y] (sum-power-levels x y)])) 
+(defn power-levels-squares [n]
+  (for [x (range (inc (- 300 n))) y (range (inc (- 300 n)))]
+    [[x y] (sum-power-levels x y n)]))
 
-(def max-power-level-square 
-  (apply max-key second power-levels-squares))
+(defn max-power-level-square [n]
+  (apply max-key second (power-levels-squares n)))
+
+(def answer1 (max-power-level-square 3))
+
+(def answer2
+  (apply max-key (comp second second)
+        (for [i (range 1 21)]
+           [i (max-power-level-square i)])))
